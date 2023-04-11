64379e137eacd436054f4d9b714143aface720a5
Jordindibbs <jordindibbs@gmail.com>
2023-04-10 21:17:28 -0700

    HTML Better
    Updated HTML

--- jQuery.js
+++ jQuery.js
@@ -1,8 +1,7 @@
-$(document).ready(() => {
+jQuery(function($) { 
     $(".obj-chart li").on("click", (event) => {
       $(event.currentTarget).addClass("active");
       $(".obj-chart").children().removeClass("disabled");
       $(event.currentTarget).siblings().removeClass("active");
     });
-  })
-  
+})(jQuery);
