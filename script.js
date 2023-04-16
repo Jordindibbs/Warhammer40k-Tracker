ad135bf09f366ec65c1dba3516fdbc7441a4f135
Jordindibbs <jordindibbs@gmail.com>
2023-04-15 19:10:11 -0700

    CSS Overhaul
    Applied multiple CSS elements, still a little work to get everything where I would like it.

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-$document.ready(function () {
-  var total = 0;
-  $("button[name=button1],[name=button2],[name=button3]").on(
-    "click",
-    function () {
-      var self = $(this);
-      var gValue = Number(self.val());
-      if (self.hasClass("add")) {
-        total += gValue;
-        self.removeClass("add").addClass("sub");
-      } else {
-        total -= gValue;
-        self.removeClass("sub").addClass("add");
-      }
+/*
+let buttonToggle = {
+  let buttons = $("button");
+  buttons.click(function () {
+    buttons.css("background-color", "#d6dbe2");
+    $(this).css("background-color", "#466995");
+  })
+}
+*/
 
-      $("#costs").val(total);
-    }
-  );
+var total = 0;
+$("radio[name=button1],[name=button2],[name=button3]").on("click", function () {
+  var self = $(this);
+  var gValue = Number(self.val());
+
+  if (self.hasClass("add")) {
+    total += gValue;
+    self.removeClass("add").addClass("sub");
+  } else {
+    total -= gValue;
+    self.removeClass("sub").addClass("add");
+  }
+  $("#costs").val(total);
 });
+
+/*
+const buttons = document.getElementById("button1");
+let player1Score = 0;
+function updateNumber() {}
+buttons.addEventListener("click", (e) => {
+  if (e.target.classList.contains("add")) {
+    player1Score += 15;
+    $("#costs").val(player1Score);
+  }
+});
+*/
